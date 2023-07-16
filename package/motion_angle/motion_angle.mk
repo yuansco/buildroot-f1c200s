8b5773dc084d2889cf4fccfa677e724d058d896b
Yu-An Chen <aazz99770@gmail.com>
2023-07-16 15:23:27 +0800

    separated motion_angle into a separate repo

--- package/motion_angle/motion_angle.mk
+++ package/motion_angle/motion_angle.mk
@@ -4,16 +4,19 @@
 #
 ################################################################################
 
-MOTION_ANGLE_VERSION = 1.0
-MOTION_ANGLE_SITE = ./package/motion_angle
-MOTION_ANGLE_SITE_METHOD = local
+MOTION_ANGLE_VERSION:=master
+MOTION_ANGLE_SITE=https://github.com/yuansco/Motion-Angle.git
+MOTION_ANGLE_SITE_METHOD=git
 
 define MOTION_ANGLE_BUILD_CMDS
-    $(MAKE) -C $(@D) $(TARGET_CONFIGURE_OPTS)
+	$(MAKE) -C $(@D) $(TARGET_CONFIGURE_OPTS)
 endef
 
 define MOTION_ANGLE_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0755 $(@D)/motion_angle $(TARGET_DIR)/usr/bin
+	$(INSTALL) -D -m 0755 $(@D)/gsensor_show.sh $(TARGET_DIR)/root
 endef
 
 $(eval $(generic-package))
+
+
